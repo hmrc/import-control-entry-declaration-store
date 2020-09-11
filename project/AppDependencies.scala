2d866c13220b66e370fc087c9d058e899ce7573a
Fabian Lewis <49647154+frobian21@users.noreply.github.com>
2020-09-11 10:29:57 +0100

    SSEE-943 - update reactivemongo

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -5,7 +5,7 @@ object AppDependencies {
 
   val compile: Seq[ModuleID] = Seq(
     "com.github.fge"         % "json-schema-validator"      % "2.2.6",
-    "uk.gov.hmrc"            %% "simple-reactivemongo"      % "7.26.0-play-26",
+    "uk.gov.hmrc"            %% "simple-reactivemongo"      % "7.30.0-play-26",
     "uk.gov.hmrc"            %% "bootstrap-backend-play-26" % "2.24.0",
     "org.typelevel"          %% "cats-core"                 % "2.0.0",
     "org.scala-lang.modules" %% "scala-xml"                 % "1.2.0",
