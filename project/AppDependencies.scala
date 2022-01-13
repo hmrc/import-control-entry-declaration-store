7395631e88caf7b78331f8f24fcb55719670eaa1
Paul Anderson <15211314+paulanderson1234@users.noreply.github.com>
2022-01-13 11:59:42 +0000

    DL-7067 dependency and plugin updates

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,7 +2,7 @@ import play.core.PlayVersion.current
 import sbt._
 
 object AppDependencies {
-  val bootstrapVersion = "5.18.0"
+  val bootstrapVersion = "5.19.0"
 
   val compile: Seq[ModuleID] = Seq(
     "com.github.java-json-tools"   %  "json-schema-validator"     % "2.2.14",
@@ -15,34 +15,34 @@ object AppDependencies {
     "com.lucidchart"               %% "xtract"                    % "2.2.1",
     "org.codehaus.groovy"          %  "groovy-all"                % "3.0.9",
     "com.chuusai"                  %% "shapeless"                 % "2.3.7",
-    "com.fasterxml.jackson.module" %% "jackson-module-scala"      % "2.13.0"
+    "com.fasterxml.jackson.module" %% "jackson-module-scala"      % "2.13.1"
   )
 
   val test: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"            %% "bootstrap-test-play-28"  % bootstrapVersion % "test, it",
     "com.typesafe.play"      %% "play-test"               % current          % "test, it",
     "org.pegdown"            %  "pegdown"                 % "1.6.0"          % "test, it",
-    "org.scalamock"          %% "scalamock"               % "5.1.0"          % "test, it",
+    "org.scalamock"          %% "scalamock"               % "5.2.0"          % "test, it",
     "org.scalacheck"         %% "scalacheck"              % "1.15.4"         % "test, it",
     "com.github.tomakehurst" %  "wiremock-jre8"           % "2.32.0"         % "test, it",
-    "org.mockito"            %  "mockito-core"            % "4.1.0"          % "test, it",
+    "org.mockito"            %  "mockito-core"            % "4.2.0"          % "test, it",
     "org.mockito"            %% "mockito-scala"           % "1.16.49"        % "test, it",
     "org.mockito"            %% "mockito-scala-scalatest" % "1.16.49"        % "test, it",
     "com.miguno.akka"        %% "akka-mock-scheduler"     % "0.5.5"          % "test, it",
-    "com.typesafe.akka"      %% "akka-testkit"            % "2.6.15"         % "test, it"
+    "com.typesafe.akka"      %% "akka-testkit"            % "2.6.18"         % "test, it"
   )
 
 // Fixes a transitive dependency clash between wiremock and scalatestplus-play
   val overrides: Seq[ModuleID] = {
     val jettyFromWiremockVersion = "9.4.44.v20210927"
     Seq(
-      "com.typesafe.akka"           %% "akka-actor"                 % "2.6.17",
-      "com.typesafe.akka"           %% "akka-stream"                % "2.6.17",
-      "com.typesafe.akka"           %% "akka-protobuf"              % "2.6.17",
-      "com.typesafe.akka"           %% "akka-slf4j"                 % "2.6.17",
-      "com.typesafe.akka"           %% "akka-serialization-jackson" % "2.6.17",
-      "com.typesafe.akka"           %% "akka-actor-typed"           % "2.6.17",
-      "com.typesafe.akka"           %% "akka-testkit"               % "2.6.17",
+      "com.typesafe.akka"           %% "akka-actor"                 % "2.6.18",
+      "com.typesafe.akka"           %% "akka-stream"                % "2.6.18",
+      "com.typesafe.akka"           %% "akka-protobuf"              % "2.6.18",
+      "com.typesafe.akka"           %% "akka-slf4j"                 % "2.6.18",
+      "com.typesafe.akka"           %% "akka-serialization-jackson" % "2.6.18",
+      "com.typesafe.akka"           %% "akka-actor-typed"           % "2.6.18",
+      "com.typesafe.akka"           %% "akka-testkit"               % "2.6.18",
       "org.eclipse.jetty"           % "jetty-client"       % jettyFromWiremockVersion,
       "org.eclipse.jetty"           % "jetty-continuation" % jettyFromWiremockVersion,
       "org.eclipse.jetty"           % "jetty-http"         % jettyFromWiremockVersion,
