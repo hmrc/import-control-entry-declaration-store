2374c7d6180556ca6d8cfa7e57a83bcba3877170
cmrhmrc <55755517+cmrhmrc@users.noreply.github.com>
2022-02-21 13:04:53 +0000

    DL-7248, support auto replay start/stop/status requests and maintain auto replay status in mongo

---
+++ app/uk/gov/hmrc/entrydeclarationstore/controllers/AutoReplayController.scala
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2022 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.entrydeclarationstore.controllers
+
+import play.api.Logging
+import play.api.libs.json.Json
+import play.api.mvc.{Action, AnyContent, ControllerComponents}
+import uk.gov.hmrc.entrydeclarationstore.services.AutoReplayService
+import uk.gov.hmrc.play.bootstrap.backend.controller.BackendController
+import javax.inject.{Inject, Singleton}
+import scala.concurrent.ExecutionContext
+
+@Singleton()
+class AutoReplayController @Inject()(cc: ControllerComponents, service: AutoReplayService)(
+  implicit ec: ExecutionContext)
+  extends BackendController(cc) with Logging {
+
+  def start(): Action[AnyContent] = Action.async { _ => service.start.map(_ => NoContent)}
+
+  def stop(): Action[AnyContent] = Action.async { _ => service.stop.map(_ => NoContent)}
+
+  def getStatus: Action[AnyContent] = Action.async { _ => service.getStatus.map(status => Ok(Json.toJson(status)))}
+}
---
+++ test/uk/gov/hmrc/entrydeclarationstore/services/MockAutoReplayService.scala
@@ -0,0 +1,38 @@
+/*
+ * Copyright 2022 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.entrydeclarationstore.services
+
+import org.scalamock.handlers.CallHandler
+import org.scalamock.scalatest.MockFactory
+import uk.gov.hmrc.entrydeclarationstore.models.AutoReplayStatus
+
+import scala.concurrent.Future
+
+trait MockAutoReplayService extends MockFactory {
+  val mockAutoReplayService: AutoReplayService = mock[AutoReplayService]
+
+  object MockAutoReplayService {
+    def start(): CallHandler[Future[Unit]] =
+      mockAutoReplayService.start _ expects ()
+
+    def stop(): CallHandler[Future[Unit]] =
+      mockAutoReplayService.stop _ expects ()
+
+    def getStatus: CallHandler[Future[AutoReplayStatus]] =
+      mockAutoReplayService.getStatus _ expects ()
+  }
+}
