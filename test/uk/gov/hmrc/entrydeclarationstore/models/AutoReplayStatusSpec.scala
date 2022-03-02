e2220e7b003f0bc46ae517d940280375f53f3365
cmrhmrc <55755517+cmrhmrc@users.noreply.github.com>
2022-03-02 10:29:59 +0000

    DL-7225, add tests

--- test/uk/gov/hmrc/entrydeclarationstore/models/AutoReplayStatusSpec.scala
+++ test/uk/gov/hmrc/entrydeclarationstore/models/AutoReplayStatusSpec.scala
@@ -18,10 +18,11 @@ package uk.gov.hmrc.entrydeclarationstore.models
 
 import org.scalatest.matchers.should.Matchers.convertToAnyShouldWrapper
 import org.scalatest.wordspec.AnyWordSpec
-
+import java.time.Instant
 import play.api.libs.json.Json
 
 class AutoReplayStatusSpec extends AnyWordSpec {
+  val now: Instant = Instant.parse("2022-03-02T10:26:38.559Z")
 
   implicit val formats = AutoReplayStatus.format
   "AutoReplayStatus" when {
@@ -76,4 +77,75 @@ class AutoReplayStatusSpec extends AnyWordSpec {
     }
 
   }
+
+  "AutoReplayStatus with ReplayState" when {
+    val replayState: ReplayState = ReplayState(now, Some(now), true, 5, 0, 5)
+    "On" must {
+      val json = Json.parse("""
+                              |{
+                              |  "autoReplay": true,
+                              |  "lastReplay": {
+                              |    "startTime": "2022-03-02T10:26:38.559Z",
+                              |    "endTime": "2022-03-02T10:26:38.559Z",
+                              |    "completed": true,
+                              |    "successCount": 5,
+                              |    "failureCount": 0,
+                              |    "totalToReplay": 5
+                              |  }
+                              |}
+                              |""".stripMargin)
+
+      "serialize to JSON correctly" in {
+        val status: AutoReplayStatus = AutoReplayStatus.On(Some(replayState))
+        Json.toJson(status) shouldBe json
+      }
+
+      "deserialize from JSON correctly" in {
+        json.as[AutoReplayStatus] shouldBe AutoReplayStatus.On(Some(replayState))
+      }
+    }
+
+    "Off" must {
+      val json = Json.parse("""
+                              |{
+                              |"autoReplay": false,
+                              |  "lastReplay": {
+                              |    "startTime": "2022-03-02T10:26:38.559Z",
+                              |    "endTime": "2022-03-02T10:26:38.559Z",
+                              |    "completed": true,
+                              |    "successCount": 5,
+                              |    "failureCount": 0,
+                              |    "totalToReplay": 5
+                              |  }
+                              |}
+                              |""".stripMargin)
+
+      "serialize to JSON correctly" in {
+        val status: AutoReplayStatus = AutoReplayStatus.Off(Some(replayState))
+        Json.toJson(status) shouldBe json
+      }
+
+      "deserialize from JSON correctly" in {
+        json.as[AutoReplayStatus] shouldBe AutoReplayStatus.Off(Some(replayState))
+      }
+    }
+
+    "Unavailable" must {
+      val json = Json.parse("""
+                              |{
+                              |}
+                              |""".stripMargin)
+
+      "serialize to JSON correctly" in {
+        val status: AutoReplayStatus = AutoReplayStatus.Unavailable
+        Json.toJson(status) shouldBe json
+      }
+
+      "deserialize from JSON correctly" in {
+        json.as[AutoReplayStatus] shouldBe AutoReplayStatus.Unavailable
+      }
+    }
+
+  }
+
 }
