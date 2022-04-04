84540e0ed1c3f2cb396f90ed4c6706c9d736670a
cmrhmrc <55755517+cmrhmrc@users.noreply.github.com>
2022-04-04 13:09:24 +0100

    DL-7407, dependency updates

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -6,19 +6,19 @@ object AppDependencies {
 
   val compile: Seq[ModuleID] = Seq(
     "com.github.java-json-tools"   %  "json-schema-validator"     % "2.2.14",
-    "uk.gov.hmrc.mongo"            %% "hmrc-mongo-play-28"        % "0.60.0",
+    "uk.gov.hmrc.mongo"            %% "hmrc-mongo-play-28"        % "0.62.0",
     "uk.gov.hmrc"                  %% "bootstrap-backend-play-28" % bootstrapVersion,
     "org.typelevel"                %% "cats-core"                 % "2.7.0",
     "org.scala-lang.modules"       %% "scala-xml"                 % "2.0.1",
     "com.lucidchart"               %% "xtract"                    % "2.2.1",
-    "org.codehaus.groovy"          %  "groovy-all"                % "3.0.9",
-    "com.chuusai"                  %% "shapeless"                 % "2.3.8",
-    "com.fasterxml.jackson.module" %% "jackson-module-scala"      % "2.13.1"
+    "org.codehaus.groovy"          %  "groovy-all"                % "3.0.10",
+    "com.chuusai"                  %% "shapeless"                 % "2.3.9",
+    "com.fasterxml.jackson.module" %% "jackson-module-scala"      % "2.13.2"
   )
 
   val test: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"            %% "bootstrap-test-play-28"  % bootstrapVersion % "test, it",
-    "uk.gov.hmrc.mongo"      %% "hmrc-mongo-test-play-28" % "0.60.0"         % "test, it",
+    "uk.gov.hmrc.mongo"      %% "hmrc-mongo-test-play-28" % "0.62.0"         % "test, it",
     "com.typesafe.play"      %% "play-test"               % current          % "test, it",
     "org.pegdown"            %  "pegdown"                 % "1.6.0"          % "test, it",
     "org.scalamock"          %% "scalamock"               % "5.2.0"          % "test, it",
@@ -28,7 +28,7 @@ object AppDependencies {
     "org.mockito"            %% "mockito-scala"           % "1.16.49"        % "test, it",
     "org.mockito"            %% "mockito-scala-scalatest" % "1.16.49"        % "test, it",
     "com.miguno.akka"        %% "akka-mock-scheduler"     % "0.5.5"          % "test, it",
-    "com.typesafe.akka"      %% "akka-testkit"            % "2.6.18"         % "test, it"
+    "com.typesafe.akka"      %% "akka-testkit"            % "2.6.19"         % "test, it"
   )
 
 // Fixes a transitive dependency clash between wiremock and scalatestplus-play
