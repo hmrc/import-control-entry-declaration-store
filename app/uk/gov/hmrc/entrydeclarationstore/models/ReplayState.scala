d3629a6e0f019b074e48a2b09e9191df39912890
cmrhmrc <55755517+cmrhmrc@users.noreply.github.com>
2022-04-05 08:48:06 +0100

    DL-7407, update ReplayState serialisation to allow for missing replay trigger field in old mongo data

--- app/uk/gov/hmrc/entrydeclarationstore/models/ReplayState.scala
+++ app/uk/gov/hmrc/entrydeclarationstore/models/ReplayState.scala
@@ -16,8 +16,8 @@
 
 package uk.gov.hmrc.entrydeclarationstore.models
 
-import play.api.libs.json.{Format, Json}
-
+import play.api.libs.json.{Format, Json, __, Reads, OWrites}
+import play.api.libs.functional.syntax._
 import java.time.Instant
 
 case class ReplayState(
@@ -31,13 +31,44 @@ case class ReplayState(
   failureCount: Int = 0)
 
 object ReplayState {
+
+  def build(id: String,
+            start: Instant,
+            total: Int,
+            trigger: Option[ReplayTrigger],
+            completed: Option[Boolean],
+            end: Option[Instant],
+            success: Int,
+            failure: Int): ReplayState =
+    ReplayState(id, start, total, trigger.getOrElse(ReplayTrigger.Manual), completed, end, success, failure)
+
   object Implicits {
     implicit val replayStateFormat: Format[ReplayState] = Json.format[ReplayState]
   }
 
   object MongoImplicits {
     import uk.gov.hmrc.mongo.play.json.formats.MongoJavatimeFormats.Implicits._
+    val reads: Reads[ReplayState] = (
+        (__ \ "replayId").read[String] and
+        (__ \ "startTime").read[Instant] and
+        (__ \ "totalToReplay").read[Int] and
+        (__ \ "trigger").readNullable[ReplayTrigger] and
+        (__ \ "completed").readNullable[Boolean] and
+        (__ \ "endTime").readNullable[Instant] and
+        (__ \ "successCount").read[Int] and
+        (__ \ "failureCount").read[Int]
+      )(build _)
 
-    implicit val mongoFormat: Format[ReplayState] = Json.format[ReplayState]
+    val writes: OWrites[ReplayState] = (
+        (__ \ "replayId").write[String] and
+        (__ \ "startTime").write[Instant] and
+        (__ \ "totalToReplay").write[Int] and
+        (__ \ "trigger").write[ReplayTrigger] and
+        (__ \ "completed").writeNullable[Boolean] and
+        (__ \ "endTime").writeNullable[Instant] and
+        (__ \ "successCount").write[Int] and
+        (__ \ "failureCount").write[Int]
+      )(unlift(ReplayState.unapply))
+    implicit val mongoFormat: Format[ReplayState] = Format(reads, writes)
   }
 }
