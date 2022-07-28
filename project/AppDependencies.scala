8e8ce00e1fc2b9263f7a8e8fb85e58b9f94cf58e
Paul Anderson <15211314+paulanderson1234@users.noreply.github.com>
2022-07-28 16:13:29 +0100

    DL-8297 upgrade hmrc-mongo plus other dependencies and plugins

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -6,19 +6,19 @@ object AppDependencies {
 
   val compile: Seq[ModuleID] = Seq(
     "com.github.java-json-tools"   %  "json-schema-validator"     % "2.2.14",
-    "uk.gov.hmrc.mongo"            %% "hmrc-mongo-play-28"        % "0.67.0",
+    "uk.gov.hmrc.mongo"            %% "hmrc-mongo-play-28"        % "0.68.0",
     "uk.gov.hmrc"                  %% "bootstrap-backend-play-28" % bootstrapVersion,
     "org.typelevel"                %% "cats-core"                 % "2.8.0",
     "org.scala-lang.modules"       %% "scala-xml"                 % "2.1.0",
     "com.lucidchart"               %% "xtract"                    % "2.2.1",
-    "org.codehaus.groovy"          %  "groovy"                    % "3.0.11",
+    "org.codehaus.groovy"          %  "groovy"                    % "3.0.12",
     "com.chuusai"                  %% "shapeless"                 % "2.3.9",
     "com.fasterxml.jackson.module" %% "jackson-module-scala"      % "2.13.3"
   )
 
   val test: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"            %% "bootstrap-test-play-28"  % bootstrapVersion % "test, it",
-    "uk.gov.hmrc.mongo"      %% "hmrc-mongo-test-play-28" % "0.67.0"         % "test, it",
+    "uk.gov.hmrc.mongo"      %% "hmrc-mongo-test-play-28" % "0.68.0"         % "test, it",
     "com.typesafe.play"      %% "play-test"               % current          % "test, it",
     "org.pegdown"            %  "pegdown"                 % "1.6.0"          % "test, it",
     "org.scalamock"          %% "scalamock"               % "5.2.0"          % "test, it",
