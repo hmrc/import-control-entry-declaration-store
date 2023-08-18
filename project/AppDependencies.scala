eb530608cb564bebd7611078922492f37abf853a
christopher.richardson <55755517+cmrhmrc@users.noreply.github.com>
2023-08-18 10:03:46 +0100

    DL-10749, dependency update, fix mockito test usage

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,29 +2,30 @@ import play.core.PlayVersion.current
 import sbt._
 
 object AppDependencies {
-  val bootstrapVersion = "5.25.0"
-  val akkaVersion = "2.6.20"
+  val bootstrapVersion = "7.21.0"
+  val akkaVersion = "2.6.21"
+  val mongoVersion = "1.3.0"
 
   val compile: Seq[ModuleID] = Seq(
     "com.github.java-json-tools"   %  "json-schema-validator"     % "2.2.14",
-    "uk.gov.hmrc.mongo"            %% "hmrc-mongo-play-28"        % "0.74.0",
+    "uk.gov.hmrc.mongo"            %% "hmrc-mongo-play-28"        % mongoVersion,
     "uk.gov.hmrc"                  %% "bootstrap-backend-play-28" % bootstrapVersion,
-    "org.typelevel"                %% "cats-core"                 % "2.9.0",
-    "org.scala-lang.modules"       %% "scala-xml"                 % "2.1.0",
+    "org.typelevel"                %% "cats-core"                 % "2.10.0",
+    "org.scala-lang.modules"       %% "scala-xml"                 % "2.2.0",
     "com.lucidchart"               %% "xtract"                    % "2.3.0",
-    "org.codehaus.groovy"          %  "groovy"                    % "3.0.17",
+    "org.codehaus.groovy"          %  "groovy"                    % "3.0.18",
     "com.chuusai"                  %% "shapeless"                 % "2.3.10",
-    "com.fasterxml.jackson.module" %% "jackson-module-scala"      % "2.15.0"
+    "com.fasterxml.jackson.module" %% "jackson-module-scala"      % "2.15.2"
   )
 
   val test: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"            %% "bootstrap-test-play-28"     % bootstrapVersion % "test, it",
-    "uk.gov.hmrc.mongo"      %% "hmrc-mongo-test-play-28"    % "0.74.0"         % "test, it",
+    "uk.gov.hmrc.mongo"      %% "hmrc-mongo-test-play-28"    % mongoVersion     % "test, it",
     "com.typesafe.play"      %% "play-test"                  % current          % "test, it",
     "org.scalamock"          %% "scalamock"                  % "5.2.0"          % "test, it",
     "org.scalacheck"         %% "scalacheck"                 % "1.17.0"         % "test, it",
     "com.github.tomakehurst" %  "wiremock-jre8"              % "2.35.0"         % "test, it",
-    "org.mockito"            %  "mockito-core"               % "4.11.0"         % "test, it",
+    "org.mockito"            %  "mockito-core"               % "5.4.0"          % "test, it",
     "org.mockito"            %% "mockito-scala"              % "1.17.14"        % "test, it",
     "org.mockito"            %% "mockito-scala-scalatest"    % "1.17.14"        % "test, it",
     "com.miguno.akka"        %% "akka-mock-scheduler"        % "0.5.5"          % "test, it",
