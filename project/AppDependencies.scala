e491867cc52230c34efe38b6d1f3f7918ad343b5
Ben Ryan <37295698+benryan1790@users.noreply.github.com>
2024-03-15 12:06:00 +0000

    further dependency updates

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,9 +2,9 @@ import play.core.PlayVersion.current
 import sbt._
 
 object AppDependencies {
-  val bootstrapVersion = "8.4.0"
+  val bootstrapVersion = "8.5.0"
   val pekkoVersion = "1.0.2"
-  val mongoVersion = "1.7.0"
+  val mongoVersion = "1.8.0"
 
   val compile: Seq[ModuleID] = Seq(
     "com.github.java-json-tools"   %  "json-schema-validator"     % "2.2.14",
@@ -13,9 +13,9 @@ object AppDependencies {
     "org.typelevel"                %% "cats-core"                 % "2.10.0",
     "org.scala-lang.modules"       %% "scala-xml"                 % "2.2.0",
     "com.lucidchart"               %% "xtract"                    % "2.3.0",
-    "org.codehaus.groovy"          %  "groovy"                    % "3.0.18",
+    "org.codehaus.groovy"          %  "groovy"                    % "3.0.21",
     "com.chuusai"                  %% "shapeless"                 % "2.3.10",
-    "com.fasterxml.jackson.module" %% "jackson-module-scala"      % "2.16.1"
+    "com.fasterxml.jackson.module" %% "jackson-module-scala"      % "2.17.0"
   )
 
   val test: Seq[ModuleID] = Seq(
@@ -23,9 +23,9 @@ object AppDependencies {
     "uk.gov.hmrc.mongo"      %% "hmrc-mongo-test-play-30"     % mongoVersion,
     "org.scalamock"          %% "scalamock"                   % "5.2.0",
     "org.scalacheck"         %% "scalacheck"                  % "1.17.0",
-    "org.wiremock"           %  "wiremock"                    % "3.3.1",
-    "org.mockito"            %  "mockito-core"                % "5.4.0",
-    "org.mockito"            %% "mockito-scala"               % "1.17.14",
+    "org.wiremock"           %  "wiremock"                    % "3.4.2",
+    "org.mockito"            %  "mockito-core"                % "5.11.0",
+    "org.mockito"            %% "mockito-scala"               % "1.17.30",
     "com.github.pjfanning"   %% "pekko-mock-scheduler"        % "0.6.0",
     "org.apache.pekko"       %% "pekko-testkit"               % pekkoVersion,
     "org.apache.pekko"       %% "pekko-actor-typed"           % pekkoVersion,
