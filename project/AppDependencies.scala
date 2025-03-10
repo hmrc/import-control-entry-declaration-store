c12ffcedb8a55a9efdfdd09f188fbc35bf288093
christianmennerich <177663202+christianmennerich@users.noreply.github.com>
2025-03-10 12:25:56 +0000

    DL-15631 - Updates dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,28 +2,28 @@ import play.core.PlayVersion.current
 import sbt._
 
 object AppDependencies {
-  val bootstrapVersion = "9.1.0"
-  val pekkoVersion = "1.0.3"
-  val mongoVersion = "2.2.0"
+  val bootstrapVersion = "9.11.0"
+  val pekkoVersion = "1.1.3"
+  val mongoVersion = "2.6.0"
 
   val compile: Seq[ModuleID] = Seq(
     "com.github.java-json-tools"   %  "json-schema-validator"     % "2.2.14",
     "uk.gov.hmrc.mongo"            %% "hmrc-mongo-play-30"        % mongoVersion,
     "uk.gov.hmrc"                  %% "bootstrap-backend-play-30" % bootstrapVersion,
-    "org.typelevel"                %% "cats-core"                 % "2.12.0",
+    "org.typelevel"                %% "cats-core"                 % "2.13.0",
     "org.scala-lang.modules"       %% "scala-xml"                 % "2.3.0",
     "com.lucidchart"               %% "xtract"                    % "2.3.0",
-    "org.codehaus.groovy"          %  "groovy"                    % "3.0.22",
-    "com.chuusai"                  %% "shapeless"                 % "2.3.12",
-    "com.fasterxml.jackson.module" %% "jackson-module-scala"      % "2.17.2"
+    "org.apache.groovy"          %  "groovy"                    % "4.0.26",
+    "com.chuusai"                  %% "shapeless"                 % "2.3.13",
+    "com.fasterxml.jackson.module" %% "jackson-module-scala"      % "2.18.3"
   )
 
   val test: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"            %% "bootstrap-test-play-30"      % bootstrapVersion,
     "uk.gov.hmrc.mongo"      %% "hmrc-mongo-test-play-30"     % mongoVersion,
-    "org.scalamock"          %% "scalamock"                   % "6.0.0",
-    "org.scalacheck"         %% "scalacheck"                  % "1.18.0",
-    "org.mockito"            %  "mockito-core"                % "5.12.0",
+    "org.scalamock"          %% "scalamock"                   % "6.2.0",
+    "org.scalacheck"         %% "scalacheck"                  % "1.18.1",
+    "org.mockito"            %  "mockito-core"                % "5.16.1",
     "org.mockito"            %% "mockito-scala"               % "1.17.37",
     "com.github.pjfanning"   %% "pekko-mock-scheduler"        % "0.6.0",
     "org.apache.pekko"       %% "pekko-testkit"               % pekkoVersion,
