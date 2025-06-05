53ec68385cafe3849dc484d9d8826c231aa4345d
preetampotdar <10691333+preetampotdar@users.noreply.github.com>
2025-06-05 15:52:51 +0100

    chore(DL-16363): Updated dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -17,7 +17,7 @@
 import sbt.*
 
 object AppDependencies {
-  val bootstrapVersion = "9.12.0"
+  val bootstrapVersion = "9.13.0"
   val pekkoVersion = "1.1.3"
   val mongoVersion = "2.6.0"
 
@@ -26,9 +26,9 @@ object AppDependencies {
     "uk.gov.hmrc.mongo"            %% "hmrc-mongo-play-30"        % mongoVersion,
     "uk.gov.hmrc"                  %% "bootstrap-backend-play-30" % bootstrapVersion,
     "org.typelevel"                %% "cats-core"                 % "2.13.0",
-    "org.scala-lang.modules"       %% "scala-xml"                 % "2.3.0",
+    "org.scala-lang.modules"       %% "scala-xml"                 % "2.4.0",
     "com.lucidchart"               %% "xtract"                    % "2.3.0",
-    "org.apache.groovy"             %  "groovy"                   % "4.0.26",
+    "org.apache.groovy"             %  "groovy"                   % "4.0.27",
     "com.chuusai"                  %% "shapeless"                 % "2.3.13",
     "com.fasterxml.jackson.module" %% "jackson-module-scala"      % "2.19.0"
   )
@@ -38,8 +38,8 @@ object AppDependencies {
     "uk.gov.hmrc.mongo"      %% "hmrc-mongo-test-play-30"     % mongoVersion,
     "org.scalamock"          %% "scalamock"                   % "7.3.2",
     "org.scalacheck"         %% "scalacheck"                  % "1.18.1",
-    "org.mockito"            %  "mockito-core"                % "5.17.0",
-    "org.mockito"            %% "mockito-scala"               % "1.17.37",
+    "org.mockito"            %  "mockito-core"                % "5.18.0",
+    "org.mockito"            %% "mockito-scala"               % "2.0.0",
     "com.github.pjfanning"   %% "pekko-mock-scheduler"        % "0.6.0",
     "org.apache.pekko"       %% "pekko-testkit"               % pekkoVersion,
     "org.apache.pekko"       %% "pekko-actor-typed"           % pekkoVersion,
